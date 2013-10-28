d20f091b8ad8fc5c62066cf4842a9ff58a994b85
Dan F-M <danfm@nyu.edu>
2013-10-28 14:39:00 -0400

    starting proposal

---
+++ project/citeulike.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+__all__ = []
+
+import re
+
+
+p = re.compile(r"([0-9]{4}\.[0-9]{4})"
+               r"|([a-zA-Z\-\.]+?/[0-9]+)"
+               r"|([a-zA-Z\-\.]+?\?[0-9]+)"
+               r"|([a-zA-Z\-\.]+?/\?[0-9]+)"
+               r"|([a-zA-Z\-\.]+?\?=[0-9]+)"
+               r"|([a-zA-Z\-\.]+?%2F[0-9]+)"
+               r"|([a-zA-Z\-\.]+?%2f[0-9]+)"
+               r"|([a-zA-Z\-\.]+?\$/\$[0-9]+)"
+               r"|([a-zA-Z\-\.]+?\.[0-9]+)"
+               r"|([a-zA-Z\-\.]+?\?papernum=[0-9]+)")
+p_pref = re.compile(r"([0-9]{4}\.[0-9]{4})")
+
+
+def get_id(r):
+    for i, el in enumerate(r):
+        if len(el):
+            if i == 0 or i == 1:
+                return el
+            elif i == 2:
+                return "/".join(el.split("?"))
+            elif i == 3:
+                return "/".join(el.split("/?"))
+            elif i == 4:
+                return "/".join(el.split("?="))
+            elif i == 5:
+                return "/".join(el.split("%2F"))
+            elif i == 6:
+                return "/".join(el.split("%2f"))
+            elif i == 7:
+                return "/".join(el.split("$/$"))
+            elif i == 8:
+                return "/".join(["".join(el.split(".")[:-1]),
+                                 el.split(".")[-1]])
+            else:
+                return "/".join(el.split("?papernum="))
+    return None
+
+
+def parse_linkouts(fn):
+    final = dict()
+    for line in open(fn):
+        article_id = line.split("|")[0]
+
+        results = p.findall(line)
+        ids = map(get_id, results)
+        if len(ids) == 0:
+            continue
+
+        if all([i == ids[0] for i in ids[1:]]):
+            final[article_id] = ids[0]
+
+        else:
+            for i in ids:
+                if len(p_pref.findall(i)):
+                    final[article_id] = i
+                    break
+
+    return final
+
+
+if __name__ == "__main__":
+    parse_linkouts("citeulike/arxiv")
